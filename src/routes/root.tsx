56d7a3efa5c51a5ad962d42a0407ce891f992a46
oliver-oloughlin <oliveroloughlin@hotmail.com>
2023-11-11 00:44:41 +0100

    auth config

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,14 @@
 import { Link, Outlet } from "@tanstack/react-router"
 import { Authenticator } from "@aws-amplify/ui-react"
 import { Amplify } from "aws-amplify"
+import "@aws-amplify/ui-react/styles.css"
 
 // Configure Amplify in index file or root file
 Amplify.configure({
   Auth: {
-    region: process.env["REGION"],
-    userPoolId: awsExports.USER_POOL_ID,
-    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+    region: "eu-north",
+    userPoolId: "eu-north-1_yLiXq078c",
+    userPoolWebClientId: "7n3e418kgbr3rlv7s33sjf48a3"
   }
 })
 
