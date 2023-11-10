ac514e1e8423b34b09562bbfccb9472812d898e5
h584974 <oliveroloughlin@hotmail.com>
2023-11-10 16:26:46 +0100

    added aws to root component

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,25 @@
 import { Link, Outlet } from "@tanstack/react-router"
+import { Authenticator } from "@aws-amplify/ui-react"
+import { Amplify } from "aws-amplify"
 
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    region: process.env["REGION"],
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+})
+
+// Root component
 export default function Root() {
   return (
-    <>
+    <Authenticator>
       <div>
         <Link to="/">Home</Link>
       </div>
       <hr />
       <Outlet />
-    </>
+    </Authenticator>
   )
 }
