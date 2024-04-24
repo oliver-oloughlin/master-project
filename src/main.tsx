95d5a5c10b7ce48312cd21d2b915166a19fdb2e1
oliver-oloughlin <oliveroloughlin@hotmail.com>
2024-04-24 13:49:22 +0200

    disabled authenticator

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import Authenticator from "./components/misc/Authenticator.tsx"
+//import Authenticator from "./components/misc/Authenticator.tsx"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
+    <App />
   </React.StrictMode>,
 )
